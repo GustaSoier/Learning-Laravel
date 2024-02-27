a6622ff5b6e37b80df56a3edbd4e9a8be2a66553
Gustavo <gsampaiosoier0@gmail.com>
2024-02-26 22:44:45 -0300

    LARAVEL
    Variações do Include

--- Introdução ao Blade/hello-world-laravel/resources/views/include.blade.php
+++ Introdução ao Blade/hello-world-laravel/resources/views/include.blade.php
@@ -1,6 +1,6 @@
 <h1>Include Hello World</h1>
 
-@Include('components.component')
+@IncludeIf('components.component', ['html'=> '<h1>Componente1</h1>'])
     <h1>Componente 1 - HTML</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores fugiat debitis reprehenderit veniam molestias tempora aliquam vel pariatur eveniet nesciunt iusto delectus dolorem id doloribus quibusdam cupiditate, similique maiores laboriosam.</p>
 @endcomponent
