e5da25f07065fce9daeff8fe3033e9dd02b7c023
Gustavo <gsampaiosoier0@gmail.com>
2024-03-19 23:00:05 -0300

    LARAVEL
    SoftDelete - EXTRA

--- CRUD/crud/app/Models/Post.php
+++ CRUD/crud/app/Models/Post.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory;
 
     protected $fillable = [
         'title',
         'content',
         'author'
     ];
+
+    use SoftDeletes;
+    use HasFactory;
 }
--- CRUD/crud/database/migrations/2024_03_13_022716_create_posts_table.php
+++ CRUD/crud/database/migrations/2024_03_13_022716_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('content');
             $table->string('author');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
